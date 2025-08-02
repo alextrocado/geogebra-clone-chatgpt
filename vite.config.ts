4f38e960a2ab34af1231426145efc608b635321f
alextrocado <mail@alexandretrocado.com>
2025-08-02 13:08:11 +0100

    alterei vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
+  root: '.',         // <-- Garante que o index.html está na raiz
   plugins: [react()],
   server: {
     port: 5173
